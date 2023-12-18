4e224a49fe502ca7e39da218a7fe0be1d1f872d7
pvhung <hungdev27102003@gmail.com>
2023-12-18 23:08:34 +0700

    add images and something :>

--- app/src/main/java/vn/tutorial/diceroller/MainActivity.kt
+++ app/src/main/java/vn/tutorial/diceroller/MainActivity.kt
@@ -1,37 +1,77 @@
 package vn.tutorial.diceroller
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCompositionContext
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import vn.tutorial.diceroller.ui.theme.DiceRollerTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DiceRollerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    DiceRollerApp()
-                }
+                    DiceRollerApp(modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentSize(Alignment.Center)
+                    )
             }
         }
     }
 }
 
 @Composable
-fun DiceRollerApp() {
-    Text(text = "Hello mui ngui")
+fun DiceRollerApp(modifier: Modifier = Modifier) {
+    var dice by remember {
+        mutableStateOf(R.drawable.dice_1)
+    }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Image(painter = painterResource(id = dice), contentDescription = null)
+
+        Button(onClick = {
+            dice = when(Random.nextInt(1, 7)) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                6 -> R.drawable.dice_6
+                else -> 1
+            }
+        }) {
+            Text(text = stringResource(id = R.string.button_name))
+        }
+    }
 }
 
 @Composable
