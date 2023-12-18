b01b1a2af7d50ec9d695f27e9dfdddd8aed4a7ee
pvhung <hungdev27102003@gmail.com>
2023-12-18 22:31:01 +0700

    'init'

---
+++ app/src/main/java/vn/tutorial/diceroller/MainActivity.kt
@@ -0,0 +1,47 @@
+package vn.tutorial.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import vn.tutorial.diceroller.ui.theme.DiceRollerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiceRollerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    DiceRollerApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DiceRollerApp() {
+    Text(text = "Hello mui ngui")
+}
+
+@Composable
+@Preview(
+    showSystemUi = true,
+    showBackground = true
+)
+fun DiceRollerAppPreview() {
+    DiceRollerTheme {
+        DiceRollerApp()
+    }
+}
+
